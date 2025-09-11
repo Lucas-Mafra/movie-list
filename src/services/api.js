e3161b9199502a8ec6f07818ccd2ef1cef81da6e
Lucas Mafra <lucas.lima.nave@gmail.com>
2025-09-10 23:22:53 -0300

    feat: api to catch image

--- src/services/api.js
+++ src/services/api.js
@@ -1,12 +1,15 @@
 import axios from "axios";
 
+export const imgPath500w = "https://image.tmdb.org/t/p/w500";
+export const imgPathOriginal = "https://image.tmdb.org/t/p/original";
+
 const baseURL = "https://api.themoviedb.org/3";
 const apiKey = import.meta.env.VITE_API_KEY;
 
 // trending
 export const fetchTrending = async (timeWindow = "day") => {
-  const res = await axios.get(
+  const { data } = await axios.get(
     `${baseURL}/trending/all/${timeWindow}?api_key=${apiKey}`
   );
-  return res;
+  return data?.results;
 };
